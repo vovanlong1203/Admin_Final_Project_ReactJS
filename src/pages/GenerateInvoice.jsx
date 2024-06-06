b3f979ac294fe6f3d2c5e6f64a690a08fdea3632
Võ Văn Long <vovanlong12032002@gmail.com>
2024-06-06 14:54:04 +0700

    fix

--- src/pages/GenerateInvoice.jsx
+++ src/pages/GenerateInvoice.jsx
@@ -1,4 +1,4 @@
-import pdfMake from 'pdfmake/build/pdfmake'
+import * as pdfMake from 'pdfmake/build/pdfmake'
 import * as pdfFonts from 'pdfmake/build/vfs_fonts'
 import { getOrderItemDetail } from "../api/service"
 
