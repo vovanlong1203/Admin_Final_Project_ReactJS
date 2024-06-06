bd8e2a21ebf2c0d963fb7b349762f597b2524686
Võ Văn Long <vovanlong12032002@gmail.com>
2024-06-06 15:42:38 +0700

    aa

--- src/pages/GenerateInvoice.jsx
+++ src/pages/GenerateInvoice.jsx
@@ -1,6 +1,15 @@
 import pdfMake from 'pdfmake/build/pdfmake'
 import { getOrderItemDetail } from "../api/service"
 
+pdfMake.fonts = {
+    Roboto: {
+        normal: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Regular.ttf',
+        bold: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Medium.ttf',
+        italics: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Italic.ttf',
+        bolditalics: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-MediumItalic.ttf'
+    }
+};
+
 export const generateInvoice = async (order) => {
     const orderItems = await getOrderItemDetail(order.id)
 
